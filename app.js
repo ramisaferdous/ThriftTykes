0d2511bed19519ab101cc8c5469f7633cfdc19e9
ramisaferdous <rifahramisa@iut-dhaka.edu>
2025-04-05 19:37:43 +0600

    idk

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.use(dashboardRoutes);
 app.use(shopRoutes);
 app.use(cartRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ||8080;
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`);
 });
