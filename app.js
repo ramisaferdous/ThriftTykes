00d8cdd0ac25b9142d9b06b879a66a5e8861cf6a
ramisaferdous <rifahramisa@iut-dhaka.edu>
2025-04-05 21:14:27 +0600

    last

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.use(dashboardRoutes);
 app.use(shopRoutes);
 app.use(cartRoutes);
 
-const port = process.env.PORT ||8080;
+const port = process.env.port||8080;
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`);
 });
